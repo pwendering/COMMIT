03b6f63319f4364979b2dc3a543e19e8b2509e7f
Philipp Wendering <philipp.wendering@uni-potsdam.de>
2021-02-26 14:02:40 +0100

    untrack RAVEN GPR conversion script

--- code/matlab/scripts/RAVEN_GPR_to_COBRA_format.m
+++ code/matlab/scripts/RAVEN_GPR_to_COBRA_format.m
@@ -1,41 +1,40 @@
 % convert RAVEN GPR rules to COBRA standard
-
+options
+clearvars -except topDir
 
 habitats = {'Soil', 'Leaf', 'Root'};
-thresholds = {'50'};%, '30'};
+
 disp('-------------------------------------------------------------------')
 disp('START')
 disp('-------------------------------------------------------------------')
-for t = thresholds
-    modelDir = fullfile('/stud/wendering/Masterthesis/DATA/models_RAVEN', ...
-        strcat('HMMer10E-', t{:}));
-    for i=1:numel(habitats)
-        disp(habitats{i})
-        disp('------')
-        workspace = fullfile(modelDir, strcat(habitats{i}, '_models_metFormulas.mat'));
-        load(workspace)
-        for j=1:numel(models)
-            model = models{j};
-            fprintf('converting %s ...\n', models{j}.id)
-            % replace every gene name by its index
-            for k=1:numel(model.genes)
-                id = model.genes{k};
-                id = regexptranslate('escape', id);
-                id = strcat(id, '\>');
-                repl = strcat('x(', num2str(k), ')');
-                model.rules = cellfun(@(x)regexprep(x, id, repl),...
-                    model.rules, 'UniformOutput', false);
-            end
-            model.rules =  cellfun(@(x)regexprep(x, 'or', '\|'),...
-                    model.rules, 'UniformOutput', false);
-            models{j} = model;
-            disp('done')
-            disp('------')
+
+modelDir = fullfile(topDir, 'data', 'models', 'raven', 'HMMer10E-50');
+
+for i=1:numel(habitats)
+    disp(habitats{i})
+    disp('------')
+    workspace = fullfile(modelDir, strcat(habitats{i}, '_models_metFormulas.mat'));
+    load(workspace)
+    for j=1:numel(models)
+        model = models{j};
+        fprintf('converting %s ...\n', models{j}.id)
+        % replace every gene name by its index
+        for k=1:numel(model.genes)
+            id = model.genes{k};
+            id = regexptranslate('escape', id);
+            id = strcat(id, '\>');
+            repl = strcat('x(', num2str(k), ')');
+            model.rules = cellfun(@(x)regexprep(x, id, repl),...
+                model.rules, 'UniformOutput', false);
         end
-        workspace = fullfile(modelDir, strcat(habitats{i}, '_models_COBRA_GPR.mat'));
-        save(workspace, 'models')
-        clear models
-        disp('-------------------------------------------------------------------')
+        model.rules =  cellfun(@(x)regexprep(x, 'or', '\|'),...
+            model.rules, 'UniformOutput', false);
+        models{j} = model;
+        disp('done')
+        disp('------')
     end
-    
+    workspace = fullfile(modelDir, strcat(habitats{i}, '_models_COBRA_GPR.mat'));
+    save(workspace, 'models')
+    clear models
+    disp('-------------------------------------------------------------------')
 end
